ac4c1886dd2cccd61cea21133b38f75faf1d828c
VultureX3 <ikhalepsky@gmail.com>
2018-10-14 10:09:12 +0300

    Inner cost counting done, so everything kinda works

--- script.py
+++ script.py
@@ -55,7 +55,6 @@ class Project:
                                     )
         self.inner_cost = spreadsheet2
 
-
     def __move_metadata(self):
         self.service.files().update(fileId=self.metadata_GS_ID,
                                     addParents=self.folder['id'],
@@ -91,39 +90,64 @@ class Project:
                 print('No Google user with email %s' % developer['email'])
             worksheet = spreadsheet.add_worksheet('timesheet', 300, 12)
             spreadsheet.del_worksheet(spreadsheet.sheet1)
-            for cell, text in zip(('A1', 'B1', 'C1', 'D1'),
-                                  ('Дата', 'Название работ', 
+            cell_list = worksheet.range('A1:D1')
+            for cell, value in zip(cell_list, ('Дата', 'Название работ', 
                                    'Объем работ, часов', 'Статус оплаты (заполняется только РП)')):
-                worksheet.update_acell(cell, text)
+                cell.value = value
+            worksheet.update_cells(cell_list, value_input_option='USER_ENTERED')
+            # for cell, text in zip(('A1', 'B1', 'C1', 'D1'),
+            #                       ('Дата', 'Название работ', 
+            #                        'Объем работ, часов', 'Статус оплаты (заполняется только РП)')):
+            #     worksheet.update_acell(cell, text)
             self.service.files().update(fileId=spreadsheet.id,
                                         addParents=self.folder['id'],
                                         fields='id, parents').execute()
             self.accounts[developer['Имя']] = spreadsheet
+
+    def __create_worksheets(self, spreadsheet):
+        for account in self.accounts:
+            ws_work = spreadsheet.add_worksheet('Работы %s' % account, 300, 12)
+            ws_work.update_cell(1, 1, '=IMPORTRANGE("%s", "timesheet!A:D")' % 
+                                self.accounts[account].id)
 
     def __update_cost(self):
         spreadsheet = self.cost
         ws_result = spreadsheet.add_worksheet('Итог', 300, 12)
         spreadsheet.del_worksheet(spreadsheet.sheet1)
 
-        for account in self.accounts:
-            ws_work = spreadsheet.add_worksheet('Работы %s' % account, 300, 12)
-            ws_work.update_cell(1, 1, '=IMPORTRANGE("%s", "timesheet!A:D")' % 
-                                self.accounts[account].id)
+        self.__create_worksheets(spreadsheet)
 
-        for cell, text in zip(('A1', 'B1', 'C1', 'D1', 'E1', 'F1'),
-                              ('Исполнитель', 'Должность', 'К оплате, часов', 'Ставка, рублей', 
+        # for cell, text in zip(('A1', 'B1', 'C1', 'D1', 'E1', 'F1'),
+        #                       ('Исполнитель', 'Должность', 'К оплате, часов', 'Ставка, рублей', 
+        #                        'К оплате, рублей', 'Примечания')):
+        #         ws_result.update_acell(cell, text)
+
+        cell_list = ws_result.range('A1:F1')
+        for cell, value in zip(cell_list, ('Исполнитель', 'Должность', 'К оплате, часов', 'Ставка, рублей', 
                                'К оплате, рублей', 'Примечания')):
-                ws_result.update_acell(cell, text)
+            cell.value = value
+        ws_result.update_cells(cell_list, value_input_option='USER_ENTERED')
+
         row = 2
         for developer in self.metadata['participants']:
-            for col, value in zip(range(1, 7), (
+            # for col, value in zip(range(1, 7), (
+            #         developer['Имя'],
+            #         developer['Должность'],
+            #         '''=СУММЕСЛИ('Работы {0}'!$D$2:$D, "Акт", 'Работы {0}'!$C$2:$C)'''.format(developer['Имя']),
+            #         developer['Ставка внешняя'],
+            #         '=$C{0}*$D{0}'.format(str(row)),
+            #         developer['Комментарии к затратам'])):
+            #     ws_result.update_cell(row, col, value)
+            cell_list = ws_result.range(row, 1, row, 6)
+            for cell, value in zip(cell_list, (
                     developer['Имя'],
                     developer['Должность'],
                     '''=СУММЕСЛИ('Работы {0}'!$D$2:$D, "Акт", 'Работы {0}'!$C$2:$C)'''.format(developer['Имя']),
                     developer['Ставка внешняя'],
                     '=$C{0}*$D{0}'.format(str(row)),
                     developer['Комментарии к затратам'])):
-                ws_result.update_cell(row, col, value)
+                cell.value = value
+            ws_result.update_cells(cell_list, value_input_option='USER_ENTERED')
             row += 1
 
         for resource in self.metadata['resources']:
@@ -135,6 +159,110 @@ class Project:
             row += 1
         ws_result.update_cell(row, 1, 'Итого')
         ws_result.update_cell(row, 5, '=СУММ(E2:E%s)' % str(row-1))
+
+    def __update_inner_cost(self):
+        spreadsheet = self.inner_cost
+        ws_result = spreadsheet.add_worksheet('Итог', 300, 26)
+        spreadsheet.del_worksheet(spreadsheet.sheet1)
+
+        self.__create_worksheets(spreadsheet)
+
+        cell_list = ws_result.range('A2:A6')
+        for cell, value in zip(cell_list, (
+                                'К оплате заказчиком по актам, рублей',
+                                'К оплате разработчикам по актам, рублей',
+                                'Фонд встреч, рублей',
+                                'Бонусный фонд, рублей',
+                                'Предприятию включая операционные расходы и фонд встреч, рублей')):
+            cell.value = value
+        ws_result.update_cells(cell_list, value_input_option='USER_ENTERED')
+
+        cell_list = ws_result.range('C2:C6')
+        for cell, value in zip(cell_list, ('=$H$12',
+                                 '=$I$12',
+                                 '',
+                                 '=((($C$2*0.9)-$C$3)/2)-$C$4',
+                                 '=$C$2-$C$3-$C$5')):
+            cell.value = value
+        ws_result.update_cells(cell_list, value_input_option='USER_ENTERED')
+
+        # for row, value, formula in zip(range(2, 7), (
+        #                         'К оплате заказчиком по актам, рублей',
+        #                         'К оплате разработчикам по актам, рублей',
+        #                         'Фонд встреч, рублей',
+        #                         'Бонусный фонд, рублей',
+        #                         'Предприятию включая операционные расходы и фонд встреч, рублей'),
+        #                         ('=$H$12',
+        #                          '=$I$12',
+        #                          '',
+        #                          '=((($C$2*0.9)-$C$3)/2)-$C$4',
+        #                          '=$C$2-$C$3-$C$5')):
+        #     ws_result.update_cell(row, 1, value)
+        #     ws_result.update_cell(row, 3, formula)
+
+        # cell_list = ws_result.range('C8', 'K8', 'Q8', 'S8')
+        # for cell, value in zip(cell_list, ('Основной ФОТ', 'Премиальный ФОТ', 'К оплате', 'Общая задолженность')):
+        #     cell.value = value
+        # ws_result.update_cells(cell_list, value_input_option='USER_ENTERED')
+
+        for col, value in zip((3, 11, 17, 19), 
+                              ('Основной ФОТ', 'Премиальный ФОТ', 'К оплате', 'Общая задолженность')):
+            ws_result.update_cell(8, col, value)
+
+        headers = ('Исполнитель', 'Должность', 'Ставка заказчику, рублей', 'Ставка разработчику, рублей',
+                   'Ставка для документов заказчику, рублей', 'По актам, часов', 'Часов для документов',
+                   'Стоимость для документов, рублей', 'К оплате разработчику по актам, рублей', '',
+                   'Вес вклада, баллы', 'Вклад с учетом веса, баллы', r'Предложение доли фонда, %%',
+                   r'Фактическая доля фонда, %%', 'Доля фонда, рублей', '',
+                   'К оплате разработчику по актам c премией, рублей', '', 'Всего часов к оплате',
+                   'Общая задолженность заказчика, рублей', 'Общая задолженность разработчику, рублей',
+                   'Общая задолженность предприятию, рублей')
+
+        # for col, value in zip(range(1, 23), headers):
+        #     ws_result.update_cell(9, col, value)
+
+        cell_list = ws_result.range('A9:V9')
+        for cell, value in zip(cell_list, headers):
+            cell.value = value
+        ws_result.update_cells(cell_list, value_input_option='USER_ENTERED')
+
+        row = 10
+        for developer in self.metadata['participants']:
+            values = (developer['Имя'],
+                      developer['Должность'],
+                      developer['Ставка внешняя'],
+                      developer['Ставка внутренняя'],
+                      '=ЕСЛИ($C10 >= 2000, 2000, $C10)',
+                      '''=СУММЕСЛИ('Работы {0}'!$D$2:$D, "Акт", 'Работы {0}'!$C$2:$C)'''.format(developer['Имя']),
+                      '=$F10*($C10/$E10)',
+                      '=$E10*$G10',
+                      '=$F10*$D10',
+                      '',
+                      '',
+                      '=ЕСЛИ($K10 > 0, ОКРУГЛ($I10 * $K10 / $K$12), 0)',
+                      '=ОКРУГЛ($L10*100/$L$12,1)',
+                      '',
+                      '=$C$5 * $N10 / 100',
+                      '',
+                      '=ЕСЛИ(ОКРУГЛ($I10+$O10, -3) >=$I10, ОКРУГЛ($I10+$O10, -3), ОКРУГЛВВЕРХ($I10, -3))',
+                      '',
+                      '''=СУММЕСЛИ('Работы {0}'!$D$2:$D, "Акт", 'Работы {0}'!$C$2:$C) + СУММЕСЛИ('Работы {0}'!$D$2:$D,
+                      "", 'Работы {0}'!$C$2:$C)'''.format(developer['Имя']),
+                      '=$S10*$C10',
+                      '=$D10*$S10',
+                      '=$T10-$U10')
+            # for col, value in zip(range(1, 23), values):
+            #     ws_result.update_cell(row, col, value)
+            cell_list = ws_result.range('A{0}:V{0}'.format(str(row)))
+            for cell, value in zip(cell_list, values):
+                cell.value = value
+            ws_result.update_cells(cell_list, value_input_option='USER_ENTERED')
+            row += 1
+
+        ws_result.update_cell(row, 1, 'Итого')
+        # for col in (6, 7, 8, 9, 11, 12, 13, 14, 15, 17, 19, 20, 21, 22):
+        for col in ('F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'Q', 'S', 'T', 'U', 'V'):
+            ws_result.update_acell('%s%s' % (col, row), '=СУММ({0}10:{0}{1})'.format(col, str(row-1)))
 
     def main(self):
         scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -154,6 +282,7 @@ class Project:
         # self.__move_metadata()
         self.__create_accounts()
         self.__update_cost()
+        self.__update_inner_cost()
 
 
 if __name__ == '__main__':
